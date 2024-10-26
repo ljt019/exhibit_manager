fb4c301a08b9569a7f2aa93c59b5b8cd85935a6e
Lucien Thomas <lthomas@sciport.org>
2024-10-26 17:49:53 -0500

    dev: working react compiler???

--- application/vite.config.ts
+++ application/vite.config.ts
@@ -2,9 +2,20 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
+const ReactCompilerConfig = {
+  target: "18",
+  runtimeModule: "react-18",
+};
+
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
-  plugins: [react()],
+  plugins: [
+    react({
+      babel: {
+        plugins: [["babel-plugin-react-compiler", ReactCompilerConfig]],
+      },
+    }),
+  ],
 
   resolve: {
     alias: {

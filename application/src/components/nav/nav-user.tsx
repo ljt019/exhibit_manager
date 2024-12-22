969fc0321dd4a4605cafa507c6f1161deb433d48
Lucien Thomas <lthomas@sciport.org>
2024-12-22 16:19:27 -0600

    dev: fixed user profile loading so there isn't layout shift

--- application/src/components/nav/nav-user.tsx
+++ application/src/components/nav/nav-user.tsx
@@ -36,9 +36,11 @@ export function NavUser() {
     isError: isUserProfileError,
   } = useGetUserProfile();
 
-  if (isUserProfileLoading || isUserProfileError || !userProfile) {
+  if (isUserProfileError) {
     return null;
   }
+
+  const isProfile = !isUserProfileLoading && userProfile;
 
   return (
     <SidebarMenu>
@@ -50,17 +52,20 @@ export function NavUser() {
               className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
             >
               <Avatar className="h-8 w-8 rounded-lg">
-                {userProfile.picture && (
-                  <AvatarImage
-                    src={userProfile.picture}
-                    alt={userProfile.name}
-                  />
+                {isProfile ? (
+                  userProfile.picture && (
+                    <AvatarImage
+                      src={userProfile.picture}
+                      alt={userProfile.name}
+                    />
+                  )
+                ) : (
+                  <AvatarFallback className="rounded-lg">CN</AvatarFallback>
                 )}
-                <AvatarFallback className="rounded-lg">CN</AvatarFallback>
               </Avatar>
               <div className="grid flex-1 text-left text-sm leading-tight">
                 <span className="truncate font-semibold">
-                  {userProfile.name}
+                  {isProfile ? userProfile.name : "Loading..."}
                 </span>
               </div>
               <ChevronsUpDown className="ml-auto size-4" />
@@ -75,17 +80,20 @@ export function NavUser() {
             <DropdownMenuLabel className="p-0 font-normal">
               <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
                 <Avatar className="h-8 w-8 rounded-lg">
-                  {userProfile.picture && (
-                    <AvatarImage
-                      src={userProfile.picture}
-                      alt={userProfile.name}
-                    />
+                  {isProfile ? (
+                    userProfile.picture && (
+                      <AvatarImage
+                        src={userProfile.picture}
+                        alt={userProfile.name}
+                      />
+                    )
+                  ) : (
+                    <AvatarFallback className="rounded-lg">CN</AvatarFallback>
                   )}
-                  <AvatarFallback className="rounded-lg">CN</AvatarFallback>
                 </Avatar>
                 <div className="grid flex-1 text-left text-sm leading-tight">
                   <span className="truncate font-semibold">
-                    {userProfile.name}
+                    {isProfile ? userProfile.name : "Loading..."}
                   </span>
                 </div>
               </div>

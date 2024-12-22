e918a964f91e71cf6dbebaa80888ef0eb491adc5
Lucien Thomas <lthomas@sciport.org>
2024-12-22 16:10:11 -0600

    dev: added validation using the validate crate

--- backend/src/models/bug_report.rs
+++ backend/src/models/bug_report.rs
@@ -1,8 +1,26 @@
 use serde::Deserialize;
+use validator::Validate;
 
-#[derive(Debug, Deserialize, PartialEq, Eq)]
+#[derive(Debug, Deserialize, PartialEq, Eq, Validate)]
 pub struct BugReport {
+    #[validate(length(
+        min = 1,
+        max = 100,
+        message = "Name must be between 1 and 100 characters"
+    ))]
     pub name: String,
+
+    #[validate(length(
+        min = 1,
+        max = 100,
+        message = "Title must be between 1 and 100 characters"
+    ))]
     pub title: String,
+
+    #[validate(length(
+        min = 1,
+        max = 250,
+        message = "Description must be between 1 and 250 characters"
+    ))]
     pub description: String,
 }
--- backend/src/models/part.rs
+++ backend/src/models/part.rs
@@ -1,11 +1,23 @@
 use crate::models::note::Note;
 use serde::{Deserialize, Serialize};
+use validator::Validate;
 
-#[derive(Debug, Serialize, Deserialize, PartialEq, Eq, Clone)]
+#[derive(Debug, Serialize, Deserialize, PartialEq, Eq, Clone, Validate)]
 pub struct Part {
     pub id: Option<i64>,
+
+    #[validate(length(
+        min = 1,
+        max = 100,
+        message = "Name must be between 1 and 100 characters"
+    ))]
     pub name: String,
+
+    #[validate(url)]
     pub link: String,
+
     pub exhibit_ids: Vec<i64>,
+
+    #[validate(nested)]
     pub notes: Vec<Note>,
 }
